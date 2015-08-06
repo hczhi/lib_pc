79fa8e353279670d53f441933b0105b161953f9b
洪成智 <364317853@qq.com>
2015-08-06 17:06:06 +0800

    test

--- modules/calendar/calendar.js
+++ modules/calendar/calendar.js
@@ -1,6 +1,7 @@
 /**
  * @require ../../common/js/base.js
 */
+
 (function(){
 	function getSolarDay(y, m) {
 				return [31, (((y % 4 === 0) && (y % 100 !== 0) || (y % 400 === 0)) ? 29 : 28), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][m];
